a3f8e954194d4d18921dd34852a85b3723b6b01b
Ahmed M Hassan <ahmdmhasn@gmail.com>
2019-05-14 12:34:56 +0200

    Add necessary file for Heroku

---
+++ src/main/java/eg/iti/et3am/controller/AdminController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eg.iti.et3am.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String showHome(Model model) {
+        return "admin";
+    }
+    
+}
