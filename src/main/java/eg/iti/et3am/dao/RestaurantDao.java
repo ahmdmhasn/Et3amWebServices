f842b5f34f77b9c2c9df41633e307afdb730ebab
Wael M Elmahask <Wael M Elmahask@DESKTOP-2KM43VR>
2019-05-17 22:13:14 +0200

    Add RestaurantDao

---
+++ src/main/java/eg/iti/et3am/dao/RestaurantDao.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eg.iti.et3am.dao;
+
+import eg.iti.et3am.model.Meals;
+import eg.iti.et3am.model.Restaurants;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author Wael M Elmahask
+ */
+public interface RestaurantDao {
+
+    String addRestaurant(Restaurants restaurant) throws Exception;
+
+    String addMeal(Meals meal) throws Exception;
+
+    Restaurants getRestaurantById(Integer id) throws Exception;
+
+    Restaurants getRestaurantDetailsById(Integer id) throws Exception;
+
+    List<Meals> getMealById(Integer id) throws Exception;
+
+    Set<Meals> getMealsById(Integer id) throws Exception;
+
+    List<Restaurants> getRestaurantsList() throws Exception;
+//
+//    boolean updateRestaurant(long id, Restaurants restaurant) throws Exception;
+//
+//    boolean deleteRestaurant(long id) throws Exception;
+}
