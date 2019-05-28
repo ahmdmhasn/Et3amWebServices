e4d17bb27a413055b733066c2fb21d46ee1ed51f
Ahmed M Hassan <ahmdmhasn@gmail.com>
2019-05-28 15:42:21 +0200

    start working on update user details

--- src/main/java/eg/iti/et3am/model/Users.java
+++ src/main/java/eg/iti/et3am/model/Users.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
+import org.codehaus.jackson.annotate.JsonIgnore;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
